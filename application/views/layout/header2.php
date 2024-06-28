b13a8052d372e13650cf8a6fd2de38bab939f09a
irchamali <id.irchamali@gmail.com>
2024-06-28 13:17:47 +0700

    Update header with add partners page

--- application/views/layout/header2.php
+++ application/views/layout/header2.php
@@ -21,9 +21,9 @@
 				<li class="dropdown"><a href="#"><span>Information</span> <i class="bi bi-chevron-down"></i></a>
 				<ul>
 				    <!-- <li><a href="<?= base_url('prosiding'); ?>">Prosiding</a></li> -->
-				    <!-- <li><a href="<?= base_url('abstrak'); ?>">Abstract</a></li> -->
 					<li><a href="<?= base_url('info'); ?>">Announcement</a></li>
 					<li><a href="<?= base_url('fees'); ?>">Conference Fees</a></li>
+					<li><a href="<?= base_url('partners'); ?>">Journal Partners</a></li>
 					<li><a href="<?= base_url('regist'); ?>">Registration</a></li>
 					<li><a href="<?= base_url('#venue'); ?>" class="nav-link scrollto">Venue</a></li>
 				</ul>
